46e8df395486aad8a2a0d1368477698f1f17c627
robson.souza <robson.souza@presidencia.gov.br>
2024-12-22 09:34:16 -0300

    chore: finish preparations

--- src/database.ts
+++ src/database.ts
@@ -4,14 +4,18 @@ import { env } from './env'
 // setup knex configuration and connect to the database
 export const config: Knex.Config = {
   // connect to sqlite database
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true, // Use null as the default value for non-integer types (e.g., boolean, string)
 
   // migrations configuration
   migrations: {
+    extension: 'ts',
     directory: './database/migrations',
   },
   seeds: {
