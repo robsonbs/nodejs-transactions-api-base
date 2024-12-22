01d5312e32bd7302a956ad72691ad59d14b27d19
robson.souza <robson.souza@presidencia.gov.br>
2024-12-22 09:53:49 -0300

    fix: ajusts of RENDER deploy

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }).then(() => {
-  console.log('Server is running on port 3000')
-})
+app
+  .listen({
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server is running on port 3000')
+  })
